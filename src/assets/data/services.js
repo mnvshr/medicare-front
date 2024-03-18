85240273804aad3b1ac57d861014f97c7f596f49
Manav <notmnvshr@gmail.com>
2024-03-18 12:52:54 +0530

    initial commit for frontend

---
+++ src/assets/data/services.js
@@ -0,0 +1,38 @@
+export const services = [
+  {
+    name: "Cancer Care",
+    desc: "Personalized treatment plans and unwavering support for every step of your journey.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Labor & Delivery",
+    desc: "Expert support and nurturing care for a safe and joyous childbirth experience.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+  {
+    name: "Heart & Vascular",
+    desc: "State-of-the-art diagnostics and innovative treatments to safeguard your heart's health.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Mental Health",
+    desc: "Compassionate therapy and personalized care to support your emotional well-being.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Neurology",
+    desc: "Specialized care and advanced treatments for neurological conditions, guided by experties.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Burn Treatment",
+    desc: "Comprehensive care for burn survivors, focused on healing and restoring quality of life.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+];
